84338e71348408ba44beb6297fc24d35f98cb32d
xaawii <xavixumg@gmail.com>
2024-05-22 15:56:38 +0200

    auth service 2

---
+++ gateway-service/src/main/java/com/xmartin/gatewayservice/controller/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.xmartin.gatewayservice.controller.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TokenDto {
+    private String token;
+}
