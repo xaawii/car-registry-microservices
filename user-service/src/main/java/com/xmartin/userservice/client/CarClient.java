9501621897ed9108756ee377b5aa6f811e25b229
xaawii <xavixumg@gmail.com>
2024-05-23 19:32:32 +0200

    deleted tokenrelay

--- user-service/src/main/java/com/xmartin/userservice/client/CarClient.java
+++ user-service/src/main/java/com/xmartin/userservice/client/CarClient.java
@@ -1,11 +1,10 @@
 package com.xmartin.userservice.client;
 
-import com.xmartin.userservice.interceptors.TokenInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "car-registry", configuration = TokenInterceptor.class)
+@FeignClient(name = "car-registry")
 public interface CarClient {
 
     @GetMapping("/concessionaire/cars/{id}")
